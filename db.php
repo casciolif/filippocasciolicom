96b024802029b82c65dc4843c4bc87e485b987af
Filippo <13235407@student.uts.edu.au>
2023-04-02 18:18:37 +1000

    Update db.php

--- db.php
+++ db.php
@@ -5,7 +5,7 @@ $db_host = "ls-649eefb945f8d96a2e7d739b9354002782c67afb.caln0ufxvryf.ap-southeas
 $db_name = "cards";
 $db_user = "dbmasteruser";
 $db_pass = "dbpass1!";
-$db_port = "3306";
+$db_port = 3306;
 
 //Create a database connection
 $conn = new mysqli($db_host, $db_user, $db_pass, $db_name, $db_port);
