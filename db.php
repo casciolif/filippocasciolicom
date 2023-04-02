2199de12c7d1cb043a60e13507b764a01d2399b1
Filippo <13235407@student.uts.edu.au>
2023-04-02 18:09:38 +1000

    Update db.php

--- db.php
+++ db.php
@@ -9,6 +9,8 @@ $db_pass = "dbpass1!";
 //Create a database connection
 $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
 
+echo "Connected successfully";
+
 //Check for errors
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
