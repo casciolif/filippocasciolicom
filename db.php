6e1ac5dd497fdab2c2ebff771b9fc3e8e438fbaf
Filippo <13235407@student.uts.edu.au>
2023-04-02 18:19:44 +1000

    Update db.php

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 
 //Database credentials
 $db_host = "ls-649eefb945f8d96a2e7d739b9354002782c67afb.caln0ufxvryf.ap-southeast-2.rds.amazonaws.com";
-$db_name = "cards";
+$db_name = "database_cards";
 $db_user = "dbmasteruser";
 $db_pass = "dbpass1!";
 $db_port = 3306;
